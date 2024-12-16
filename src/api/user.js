d617240ceb544b1e16259ad66777cfd215b0ed2f
Richy <wuzi50115@gmail.com>
2024-12-16 20:21:13 +0800

    feat: implement user login API using axios

---
+++ src/api/user.js
@@ -0,0 +1,10 @@
+import axios from "axios"
+export const userApi = {
+  login: async (username, password) => {
+    const { data } = await axios.post('https://dummyjson.com/user/login', {
+      username,
+      password,
+    })
+    return data
+  }
+}
